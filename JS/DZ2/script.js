8abd4bf393eea65ca9820903a6a67d66eaacc5db
Andrey19978 <135252366+Andrey19978@users.noreply.github.com>
2023-08-16 13:53:32 +0300

    DZ2

---
+++ JS/DZ2/script.js
@@ -0,0 +1,24 @@
+// Первое задание
+let nam1 = Number (1);
+let nam2 = Number (3);
+let nam3 = nam1 + nam2 ;
+alert(nam3);
+// Второе задание
+let nan1 = Number (5); //цена товара
+let nan2 = Number (7); //колличество товара
+let nan3 = Number (100); //умножение на 100
+let nan4 = Number (14); //процент
+let nan5 = nan1 * nan2 / nan3 * nan4 ;
+alert(nan5);
+// Третье задание
+let nap1 = Number (26); //температура Цельсия
+let nap2 = Number ( (`${nap1}`)*9/5+32 ); //температура Фарентгейта
+alert(nap2);
+// Четвертое задание
+let nam = prompt('Здравствуйте, как вас завут?');
+alert( 'Привет,' + (`${nam}`) + '!' );
+// Пятое задание
+let nar1 = Number (4); //ширина
+let nar2 = Number (7); //высота
+let nar3 = nar1 * nar2 ; //площадь
+alert(nar3);
