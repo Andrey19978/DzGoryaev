ec974cb56765bddf72da4cb75ce836e222a08857
Andrey19978 <andros.g@mail.ru>
2023-08-17 21:14:52 +0300

    3

---
+++ JS/lesson3/script.js
@@ -0,0 +1,33 @@
+let nam = Number(prompt('Первое число'));
+let nam1 = Number (prompt('Второе число'));
+let nam3 = nam > nam1
+console.log(nam3);
+
+let nan = Number(prompt('Первое число'));
+let nan1 = Number (2);
+let nan3 = nan <= nan1 ;
+console.log(nan3);
+
+
+let nap = Number(prompt('Первое число'));
+let nap1 = Number (prompt('Второе число'));
+if (nap > nap1) {alert(  );}
+
+
+//Первое задание
+let age = Number(prompt('Первое число'));
+let result = "Доступ разрешен!"
+let result1 = "Доступ запрещен!"
+if (age >= 18) {
+    alert (result);
+} else {(age <18) 
+    alert(result1);}
+
+//Второе задание
+let admin = String(prompt('Первое число'));
+let resul = "Добро пожаловать администратор!"
+let resul1 = "Доступ запрещен!"
+if (admin === "adminn") {
+    alert (resul);
+} else{
+    alert (DODDS);}
