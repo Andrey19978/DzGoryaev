23223513021fe3c0947a4259c524064f6e88e187
Andrey19978 <135252366+Andrey19978@users.noreply.github.com>
2023-08-22 17:59:48 +0300

    test

--- JS/lesson3/script.js
+++ JS/lesson3/script.js
@@ -30,4 +30,41 @@ let resul1 = "Доступ запрещен!"
 if (admin === "adminn") {
     alert (resul);
 } else{
-    alert (DODDS);}
+    alert (resul1);}
+//третье задание
+let num1 = Number(prompt('Первое число'));
+let num2 = Number(prompt('Второе число'));
+let oven0r00dd = (num1 % num2 == 0);
+if (oven0r00dd) {
+    alert ("Четное");
+} else{
+    alert ("не четное");}
+    //четвертое задание
+let telA = Number(prompt('число a'));
+let telB = Number(prompt('число b'));
+let compREsult = (telA > telB) ? 'Число a больше b': 'Число b больше а'; (telA === telB) ? 'Число a равно b':
+alert (compREsult);
+    //govnokod nije
+    document.querySelector("#root > div > div:nth-child(2) > div.absolute.w-full.h-full.z-10 > div > div.absolute.left-1\\/2.top-1\\/2.-translate-x-1\\/2.-translate-y-1\\/2 > div > div:nth-child(144) > div > div > div > div").click();
+    document.querySelector("#root > div > div:nth-child(2) > div.absolute.w-full.h-full.z-10 > div > div.absolute.left-1\\/2.top-1\\/2.-translate-x-1\\/2.-translate-y-1\\/2 > div > div:nth-child(129) > div > div > div > div").click();
+    document.querySelector("#root > div > div:nth-child(2) > div.absolute.w-full.h-full.z-10 > div > div.absolute.left-1\\/2.top-1\\/2.-translate-x-1\\/2.-translate-y-1\\/2 > div > div:nth-child(128) > div > div > div > div").click();
+    if (document.querySelector("#root > div > div:nth-child(2) > div.absolute.w-full.h-full.z-10 > div > div.absolute.left-1\\/2.top-1\\/2.-translate-x-1\\/2.-translate-y-1\\/2 > div > div:nth-child(128) > div > div > div > div").click()) {document.querySelector("#root > div > div:nth-child(2) > div.absolute.w-full.h-full.z-10 > div > div.absolute.left-1\\/2.top-1\\/2.-translate-x-1\\/2.-translate-y-1\\/2 > div > div:nth-child(128) > div > div > div > div").click();}
+    while (document.querySelector("#root > div > div:nth-child(2) > div.absolute.w-full.h-full.z-10 > div > div.absolute.left-1\\/2.top-1\\/2.-translate-x-1\\/2.-translate-y-1\\/2 > div > div:nth-child(128) > div > div > div > div").click()) { // выводит 0, затем 1, затем 2
+        
+        document.querySelector("#root > div > div:nth-child(2) > div.absolute.w-full.h-full.z-10 > div > div.absolute.left-1\\/2.top-1\\/2.-translate-x-1\\/2.-translate-y-1\\/2 > div > div:nth-child(128) > div > div > div > div").click();
+      }
+    
+      let tell = document.querySelector("#root > div > div:nth-child(2) > div.absolute.w-full.h-full.z-10 > div > div.absolute.left-1\\/2.top-1\\/2.-translate-x-1\\/2.-translate-y-1\\/2 > div > div:nth-child(128) > div > div > div > div").click();;
+      do {
+        tell.clock <2;
+      } while (tell++);
+      document.querySelector("#root > div > div:nth-child(2) > div.absolute.w-full.h-full.z-10 > div > div.absolute.left-1\\/2.top-1\\/2.-translate-x-1\\/2.-translate-y-1\\/2 > div > div:nth-child(129) > div > div > div > div").click();
+      document.querySelector("#root > div > div:nth-child(2) > div.absolute.w-full.h-full.z-10 > div > div.absolute.left-1\\/2.top-1\\/2.-translate-x-1\\/2.-translate-y-1\\/2 > div > div:nth-child(129) > div > div > div > div").click();
+      document.querySelector("#root > div > div:nth-child(2) > div.absolute.w-full.h-full.z-10 > div > div.absolute.left-1\\/2.top-1\\/2.-translate-x-1\\/2.-translate-y-1\\/2 > div > div:nth-child(129) > div > div > div > div").click();
+      document.querySelector("#root > div > div:nth-child(2) > div.absolute.w-full.h-full.z-10 > div > div.absolute.left-1\\/2.top-1\\/2.-translate-x-1\\/2.-translate-y-1\\/2 > div > div:nth-child(128) > div > div > div > div").click();
+      document.querySelector("#root > div > div:nth-child(2) > div.absolute.w-full.h-full.z-10 > div > div.absolute.left-1\\/2.top-1\\/2.-translate-x-1\\/2.-translate-y-1\\/2 > div > div:nth-child(128) > div > div > div > div").click();
+      document.querySelector("#root > div > div:nth-child(2) > div.absolute.w-full.h-full.z-10 > div > div.absolute.left-1\\/2.top-1\\/2.-translate-x-1\\/2.-translate-y-1\\/2 > div > div:nth-child(128) > div > div > div > div").click();
+      document.querySelector("#root > div > div:nth-child(2) > div.absolute.w-full.h-full.z-10 > div > div.absolute.left-1\\/2.top-1\\/2.-translate-x-1\\/2.-translate-y-1\\/2 > div > div:nth-child(130) > div > div > div > div.absolute.w-full.h-full.cursor-pointer.hover\\:img-highlight").click();
+      document.querySelector("#root > div > div:nth-child(2) > div.absolute.w-full.h-full.z-10 > div > div.absolute.left-1\\/2.top-1\\/2.-translate-x-1\\/2.-translate-y-1\\/2 > div > div:nth-child(130) > div > div > div > div.absolute.w-full.h-full.cursor-pointer.hover\\:img-highlight").click();
+      document.querySelector("#root > div > div:nth-child(2) > div.absolute.w-full.h-full.z-10 > div > div.absolute.left-1\\/2.top-1\\/2.-translate-x-1\\/2.-translate-y-1\\/2 > div > div:nth-child(130) > div > div > div > div.absolute.w-full.h-full.cursor-pointer.hover\\:img-highlight").click();
+      document.querySelector("#root > div > div:nth-child(2) > div.absolute.w-full.h-full.z-10 > div > div.absolute.left-1\\/2.top-1\\/2.-translate-x-1\\/2.-translate-y-1\\/2 > div > div:nth-child(116) > div > div > div > div").click();
