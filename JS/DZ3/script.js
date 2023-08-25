452e04da994b0063c24b9f789a47ac7c9ab3ec60
Andrey19978 <135252366+Andrey19978@users.noreply.github.com>
2023-08-25 11:22:36 +0300

    dz

--- JS/DZ3/script.js
+++ JS/DZ3/script.js
@@ -1,4 +1,4 @@
-//Первое задание
+//1 задание
 let age = Number(prompt('Первое число'));
 let result = "Доступ разрешен!"
 let result1 = "Доступ запрещен!"
@@ -9,7 +9,7 @@ if (age >= 18) {
     alert(result1);
 }
 
-//Второе задание
+//2 задание
 let admin = String(prompt('Первое число'));
 let resul = "Добро пожаловать администратор!"
 let resul1 = "Доступ запрещен!"
@@ -18,7 +18,7 @@ if (admin === "adminn") {
 } else {
     alert(resul1);
 }
-//третье задание
+//3 задание
 let num1 = Number(prompt('Первое число'));
 let num2 = Number(prompt('Второе число'));
 let oven0r00dd = (num1 % num2 == 0);
@@ -27,25 +27,25 @@ if (oven0r00dd) {
 } else {
     alert("не четное");
 }
-//четвертое задание
+//4 задание
 let telA = Number(prompt('число a'));
 let telB = Number(prompt('число b'));
 let compREsult = (telA > telB) ? 'Число a больше b' : (telA < telB) ? 'Число b больше а' : 'Число a равно b';
 alert(compREsult);
-//пятое задание
+//5 задание
 let isStudent = prompt("Ты студент")
 let hasJob = prompt("Ты работаешь")
 let bothConditions = (isStudent && hasJob)
 console.log(bothConditions);
-//шестое задание
+//6 задание
 let tad = Number(prompt('Введите вашь балл по экзамену'));
 let compREsul = (tad >= 60) ? 'Вы сдали' : 'Вы не сдали';
 alert(compREsul);
-//седьмое задание
+//7 задание
 let telC = Number(prompt('число a'));
 let compREsu = (telC < 18) ? 'Вы несовершеннолетний' : (telC <= 65) ? 'Вы взрослый' : 'Вы пожилой человек';
 alert(compREsu);
-//восьмое задание
+//8 задание
 let num4 = Number(prompt('Первое число'));
 let num5 = Number(prompt('Второе число'));
 let oven0r00d = (num4 % num5 == 0);
@@ -54,24 +54,24 @@ if (oven0r00d) {
 } else {
     alert("не четное");
 }
-//девятое задание
+//9 задание
 let telS = prompt('Выберите напиток');
 let compREsultD = (telS == "чай") ? 'Вы выбрали чай' : (telS == "кофе") ? 'Вы выбрали кофе' : (telS == "сок") ? 'Вы выбрали сок' : "нет такого";
 alert(compREsultD);
-//десятое задание
+//10 задание
 let telM = Number(prompt('Введите расстояние'));
 let compREsu1 = (telM < 5) ? 'Вы можете пройти пешком' : (telM <= 20) ? 'Вы можете взять велосипед' : 'Воспользуйтесь автобусом';
 alert(compREsu1);
-//одинадцатое задание
+//11 задание
 let telP = Number(prompt('введите число'));
 let telK = Number(prompt('введите число'));
 let compREsu2 = (telP < telK) ? 'Число ' + (`${telK}`) + ' больше числа ' + (`${telP}`) : (telP > telk) ? ' Число ' + (`${telP}`) + ' больше числа ' + (`${telK}`) : 'Не могу посчитать(';
 alert(compREsu2);
-//двенадцатое задание
+//12 задание
 let telY = Number(prompt('Введите число баллов по тесту'));
 let compREsua = (telY >= 90) ? 'отлично' : (telY >= 70) ? 'хорошо' : 'удовлетворительно';
 alert(compREsua);
-//тренадцатое задание
+//13 задание
 let num4 = Number(prompt('Первое число'));
 let num5 = Number(prompt('Второе число'));
 let oven0r00d = (num4 % num5 == 0);
@@ -80,11 +80,11 @@ if (oven0r00d) {
 } else {
     alert("не четное");
 }
-//четырнадцатое задание
+//14 задание
 let telZ = prompt('Выберите язык');
 let compREsultR = (telZ == "Французский") ? 'Bonjour' : (telZ == "английский") ? 'Hello' : "нет такого";
 alert(compREsultR);
-//пятнадцатое задание
+//15 задание
 let telna = prompt('Введите месяц');
 let month = (telna == "декабрь" || telna == "январь" || telna == "февраль") ? "Зима" : (telna == "март" || telna == "апрель" || telna == "май") ? "весна" : (telna == "июнь" || telna == "июль" || telna == "август") ? "лето" : (telna == "декабрь" || telna == "январь" || telna == "февраль") ? "осень" : "нет такого";
 alert(month);
