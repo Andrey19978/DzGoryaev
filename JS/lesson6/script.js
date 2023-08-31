3174d0b74ddd91910302e61e4716326c8bf694ff
Andrey19978 <andros.g@mail.ru>
2023-08-31 20:58:58 +0300

    test

---
+++ JS/lesson6/script.js
@@ -0,0 +1,20 @@
+let age = Number(prompt('введите возраст'));
+function showMessage(agex) {
+    if (agex <= 18) {
+        alert( 'школьник' );
+      } else {
+        alert( 'студент' );
+      }
+}
+showMessage(age);
+
+let telA = Number(prompt('число a'));
+let telB = Number(prompt('число b'));
+function checkNumb(a,b){
+    if (a > b) {
+        return b;
+    }else if(a < b){
+        return a;
+    };
+}
+console.log(checkNumb(telA, telB))
