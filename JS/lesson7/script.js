af904e55ccf18c089147fd008ad0d68e5435b31a
Andrey19978 <andros.g@mail.ru>
2023-09-05 21:10:19 +0300

    test

---
+++ JS/lesson7/script.js
@@ -0,0 +1,17 @@
+// let fruits = ["Яблоки", "Груша", "Апельсин"];
+
+// // добавляем новое значение в "копию"
+// let shoppingCart = fruits;
+// shoppingCart.push("Банан");
+
+// // что в fruits?
+// alert( fruits.length ); // ?
+// alert( fruits[3] );
+let x = +prompt("Введите число А");
+let y = +prompt("Введите число Б");
+function calculate(x, y) {
+ let callback = x + y;
+ return x + y;
+}
+
+calculate(x,y);
