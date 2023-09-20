1f3f4257c33b49781e633142fb1d0f779616be41
Andrey19978 <135252366+Andrey19978@users.noreply.github.com>
2023-09-20 20:48:49 +0300

    DZ8

--- JS/DZ8/script.js
+++ JS/DZ8/script.js
@@ -1,4 +1,32 @@
-
-function generateLink(){
-
+function sum( x, y){
+    return x + y;
 };
+
+let mult=function( t, m){
+   return t * m;
+};
+
+function calculate(sum, mult){
+   if( sum > mult)
+   return sum;
+   else if (sum < mult)
+   return mult;
+}
+
+calculate(sum(), mult());
+
+function calculate(a, b){
+    if( a > b)
+    return a;
+    else if (a < b)
+    return b;
+}
+function sum(callback){
+    let num = 6;
+    callback(num)
+}
+function mult(callbac){
+    let num1 = 2;
+    callbac(num1)
+}
+calculate(sum(), mult());
