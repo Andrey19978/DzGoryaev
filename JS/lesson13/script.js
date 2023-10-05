142c53b63b39bb821713db0ff50dc4fcc7c6fd17
Andrey19978 <andros.g@mail.ru>
2023-10-05 18:17:51 +0300

    lesson15

--- JS/lesson13/script.js
+++ JS/lesson13/script.js
@@ -12,3 +12,19 @@ const menu = {
   }
   multiplyNumeric(menu)
   console.log(menu)
+
+    //пузырьковая сортировка
+    let array2 = [2,6,8,3,7,9,4,77,44,33]
+    array2.sort((a,b) => a-b)
+    console.log(array2)
+    
+/////////////
+    let users = [
+      {id: 1, name: "Вася"},
+      {id: 2, name: "Петя"},
+      {id: 3, name: "Маша"}
+    ];
+    
+    let user = users.find(item => item.id == 1);
+    
+    alert(user.name); 
---
+++ JS/lesson15/script.js
@@ -0,0 +1,22 @@
+let users = [
+    { name: "Вася"},
+    { name: "Петя"},
+    { name: "Маша"}
+  ];
+
+let name = document.getElementById('name')
+  const elem1 = document.querySelector("body > form > input[type=text]:nth-child(2)");
+  const elem3 = document.querySelector("body > form > input[type=text]:nth-child(4)");
+  const elem4 = document.querySelector("body > form > input[type=text]:nth-child(6)");
+  const elem5 = document.querySelector("body > form > input[type=text]:nth-child(8)");
+  const elem2 = document.querySelector("#result");
+
+
+function save(name, age, city, number){
+
+};
+  
+  
+  
+  
+  
