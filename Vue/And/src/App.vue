a3c3007309367cf381aec9169ef804bee21d8dd7
Andrey19978 <andros.g@mail.ru>
2023-12-07 21:04:28 +0300

    |Vue

--- Vue/And/src/App.vue
+++ Vue/And/src/App.vue
@@ -1,36 +1,11 @@
 <template>
-  <div>
-    <button @click="pushMsg()">Click</button>
-    <p>{{ msg }}</p>
-  </div>
+  <nav>
+  <RouterLink to="/">Home</RouterLink>
+  <RouterLink to="/about">About</RouterLink>
+  </nav>
+  <RouterView />
 </template>
 
-<script>
-export default {
-  data () {
-    return {
-      msg: ''
-    }
-  },
-  methods: { 
-    pushMsg() {
-      this.msg = 'uyuigigiyghuikhjiguyhugy'
-    }
-  }
-}
-</script>
-
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-npm run serveshadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
---
+++ Vue/And/src/views/HomeView.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <h1>Главная</h1>
+    </div>
+</template>
+
+<script>
+import ProductCard from "./AboutView.vue";
+export default {
+    data () {
+        return {
+            product: [{
+          id: 1,
+          name: "Product 1",
+          description: "Description for product 1",
+          price: 100,
+          image: "path/to/image1.jpg",
+        },
+        {
+          id: 2,
+          name: "Product 2",
+          description: "Description for product 2",
+          price: 200,
+          image: "path/to/image2.jpg",
+        },
+        // add more products
+    ],
+        }
+    },
+    methods: {
+
+    },
+    async beforeMount(){
+
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
