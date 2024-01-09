b0afe282c4b8e7b5344520d39f3bbc469c03d456
Andrey19978 <andros.g@mail.ru>
2024-01-09 21:07:37 +0300

    vue

---
+++ fedj/client/pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="text-center text-teal-950" id="DiTe">
+        <h1 >{{ num }}</h1>
+        <button @click="myCount" id="BuDi">count </button>
+    </div>
+</template>
+
+<script setup>
+
+let title = ref("Главная страница")
+let num = ref(33)
+const myCount = () => {
+    num.value++
+}
+
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ fedj/layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+    <header class="bg-cyan-600">
+
+    </header>
+    <main class="container bg-gray-100 mx-auto">
+        <slot />
+    </main>
+    <footer>
+
+    </footer>
+</template>
+<script setup>
+
+</script>
+<style scoped>
+</style>
