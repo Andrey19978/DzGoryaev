339b9381aa33c2cd5ae166279ca1a5bdee1b2f9c
Andrey19978 <andros.g@mail.ru>
2024-01-11 21:11:53 +0300

    Blog

--- fedj/client/pages/index.vue
+++ fedj/client/pages/index.vue
@@ -1,8 +1,24 @@
 <template>
+    <header class="bg-emerald-950 text-white shadow-xl w-full p-6 flex gap-2 justify-between">
+        <nav class="space-x-2">
+            <MuxLink to="/" class="border-2 rounded p-1 hover:text-teal-100 hover:border-teal-100">Главная</MuxLink>
+            <MuxLink to="/about" class="border-2 rounded p-1 hover:text-teal-100 hover:border-teal-100">О нас</MuxLink>
+        </nav>
+    </header>
+    <main class="container bg-gray-100 mx-auto flex-auto">
+        <slot />
+    </main>
     <div class="text-center text-teal-950" id="DiTe">
         <h1 >{{ num }}</h1>
         <button @click="myCount" id="BuDi">count </button>
     </div>
+    
+<footer class="bg-emerald-950 text-white shadow-xl w-full p-6 flex gap-2 justify-between" >
+    <nav class="space-x-2">
+            <MuxLink to="/" class="border-2 rounded p-1 hover:text-teal-100 hover:border-teal-100">Главная</MuxLink>
+            <MuxLink to="/about" class="border-2 rounded p-1 hover:text-teal-100 hover:border-teal-100">О нас</MuxLink>
+        </nav>
+</footer>
 </template>
 
 <script setup>
--- fedj/layouts/default.vue
+++ fedj/layouts/default.vue
@@ -1,6 +1,9 @@
 <template>
-    <header class="bg-cyan-600">
-
+    <header class="bg-emerald-950 text-white shadow-xl w-full p-6 flex gap-2 justify-between">
+        <nav class="space-x-2">
+            <MuxLink to="/" class="border-2 rounded p-1 hover:text-teal-100 hover:border-teal-100">Главная</MuxLink>
+            <MuxLink to="/about" class="border-2 rounded p-1 hover:text-teal-100 hover:border-teal-100">О нас</MuxLink>
+        </nav>
     </header>
     <main class="container bg-gray-100 mx-auto">
         <slot />
