be6b1d3213069d7ef7b9e7c69a35514ccf196cf2
Andrey19978 <135252366+Andrey19978@users.noreply.github.com>
2024-06-14 19:40:27 +0300

    dd

---
+++ JS/my-app/src/components/header.js
@@ -0,0 +1,16 @@
+import React, { Components } from "react"
+import { Navbar } from 'react-bootstrap'
+
+export default class header extends Component {
+    render() {
+        return(
+            <Navbar>
+                <Container>
+                    <Navbar.Brand >
+
+                    </Navbar.Brand>
+                </Container>
+            </Navbar>
+        )
+    }
+}
